10259e3d5831c9a67e57ecac5c2e799dac4db72d
Pitzer34 <peter00880@gmail.com>
2022-10-24 20:04:17 +0800

    change-1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,13 @@ namespace ConsoleApp1
         {
             //宣告一個 List<Product> 變數, 存放多筆購買商品
 
-            List<Product> Product01 = new List<Product>();
-            List<Product> Product02 = new List<Product>();
-            List<Product> Product03 = new List<Product>();
+            List<Product> Product = new List<Product>()
+            {
+            new Product(){Name="1",Price=1},
+            new Product(){Name="2",Price=2},
+            new Product(){Name="3",Price=3},
+
+            };
 
         }
     }
