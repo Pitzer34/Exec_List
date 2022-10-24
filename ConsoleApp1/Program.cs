377a5080cd4c0b4617b1760d9fff96916eea6333
Pitzer34 <peter00880@gmail.com>
2022-10-24 19:33:31 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //宣告一個 List<Product> 變數, 存放多筆購買商品
+
+            List<Product> Product01 = new List<Product>();
+            List<Product> Product02 = new List<Product>();
+            List<Product> Product03 = new List<Product>();
+
+        }
+    }
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
